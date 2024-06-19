0c166527fac117f83788e615ae54aaeee7beb193
Oph3Z <59979752+Oph3Z1@users.noreply.github.com>
2024-06-19 23:57:29 +0200

    1.0.0

--- client.lua
+++ client.lua
@@ -3,7 +3,6 @@ function PlayerLook(Player, TargetPlayer)
     local TargetCoords = GetEntityCoords(TargetPlayer, true)
     local PlayerHeading = GetEntityHeading(Player)
     local TargetHeading = GetHeadingFromVector_2d(TargetCoords.x - PlayerCoords.x, TargetCoords.y - PlayerCoords.y)
-
     local HeadingDifference = math.abs(PlayerHeading - TargetHeading)
 
     return HeadingDifference < 90 or HeadingDifference > 270
@@ -12,14 +11,12 @@ end
 Citizen.CreateThread(function()
     while true do
         Citizen.Wait(0)
-
         local Player = PlayerPedId()
 
         for k, v in ipairs(GetActivePlayers()) do
             local TargetPlayer = GetPlayerPed(v)
             if TargetPlayer ~= Player then
                 local IsFacing = PlayerLook(Player, TargetPlayer)
-
                 if not IsFacing then
                     SetEntityAlpha(TargetPlayer, 100, false)
                 else
